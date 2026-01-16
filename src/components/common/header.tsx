fdca819fc24d6166c6514bb51b97e1c92552ce25
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2026-01-16 11:59:11 +0700

    feat: enhance header with navigation links and improve login page SEO

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -7,6 +7,15 @@ import { Logo } from '@/components/common/logo';
  * Libs
  */
 import { cn } from '@/lib/utils';
+import { NavLink } from 'react-router';
+
+/**
+ * Types
+ */
+type INavLink = {
+  label: string;
+  to: string;
+};
 
 type HeaderProps = {
   className?: string;
@@ -16,11 +25,44 @@ export const Header = ({ className }: HeaderProps) => {
   return (
     <header
       className={cn(
-        'border-primary bg-background mx-auto max-w-6xl border-2 p-3',
+        'border-primary bg-background mx-auto flex max-w-6xl items-center justify-between border p-3',
         className,
       )}
     >
       <Logo />
+      <NavBar className='px-5' />
     </header>
+  );
+};
+
+const navLinks: INavLink[] = [
+  {
+    label: 'Home',
+    to: '#',
+  },
+  {
+    label: 'Services',
+    to: '#',
+  },
+  {
+    label: 'About Us',
+    to: '#',
+  },
+];
+
+const NavBar = ({ className }: { className?: string }) => {
+  return (
+    <nav className={cn('', className)}>
+      <div className='flex items-center gap-4'>
+        {navLinks.map(({ label, to }) => (
+          <NavLink
+            key={label}
+            to={to}
+          >
+            {label}
+          </NavLink>
+        ))}
+      </div>
+    </nav>
   );
 };
--- src/features/auth/pages/login-page.tsx
+++ src/features/auth/pages/login-page.tsx
@@ -7,22 +7,26 @@ import loginImage from '../assets/login-image.svg';
  * Components
  */
 import LoginForm from '../components/login-form';
+import { Seo } from '@/components/common/seo';
 
 export const LoginPage = () => {
   return (
-    <div className='flex flex-row items-center gap-20'>
-      <figure className='select-none'>
-        <img
-          src={loginImage}
-          className=''
-          width={600}
-          height={600}
-          alt='Login Image'
-        />
-      </figure>
-      <div className=''>
-        <LoginForm />
+    <>
+      <Seo title='Login'  />
+      <div className='flex flex-row items-center gap-20'>
+        <figure className='select-none'>
+          <img
+            src={loginImage}
+            className=''
+            width={600}
+            height={600}
+            alt='Login Image'
+          />
+        </figure>
+        <div className=''>
+          <LoginForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
