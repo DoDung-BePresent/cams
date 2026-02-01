1c3b3b66f99a6383cf84abaa589bd78a038f5065
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2026-02-01 22:39:13 +0700

    feat: enhance AppHeader with notification, messaging, and settings buttons; add fullscreen toggle functionality

--- src/layouts/DashboardLayout/components/AppHeader.tsx
+++ src/layouts/DashboardLayout/components/AppHeader.tsx
@@ -1,14 +1,26 @@
 /**
  * Node modules
  */
-import { Button, Layout } from 'antd';
+import { Avatar, Badge, Button, Flex, Layout } from 'antd';
 import {
+  BellOutlined,
+  FullscreenExitOutlined,
+  FullscreenOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  MessageOutlined,
+  SettingOutlined,
 } from '@ant-design/icons';
+
+/**
+ * Assets
+ */
+import avatarImage from '@/assets/images/avatar-1.png';
+
+/**
+ * Hooks
+ */
+import { useFullscreen } from '@/shared/hooks/useFullscreen';
 
 /**
  * Types
@@ -27,21 +39,84 @@ const headerStyle: React.CSSProperties = {
   display: 'flex',
   alignItems: 'center',
   paddingInline: 10,
+  paddingInlineEnd: 20,
 };
 
 export const AppHeader = ({ collapsed, onClick }: AppHeaderProps) => {
+  const { isFullscreen, toggleFullscreen } = useFullscreen();
   return (
     <Header style={headerStyle}>
-      <Button
-        type='text'
-        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        onClick={onClick}
-        style={{
-          fontSize: '16px',
-          width: 36,
-          height: 36,
-        }}
-      />
+      <Flex
+        align='center'
+        justify='space-between'
+        className='w-full'
+      >
+        <Button
+          type='text'
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={onClick}
+          style={{
+            fontSize: '16px',
+            width: 36,
+            height: 36,
+          }}
+        />
+        <Flex gap='small'>
+          <Button
+            type='text'
+            icon={
+              <Badge
+                size='small'
+                color='blue'
+                count={5}
+              >
+                <BellOutlined style={{ fontSize: '16px' }} />
+              </Badge>
+            }
+            style={{
+              width: 36,
+              height: 36,
+            }}
+          />
+          <Button
+            type='text'
+            icon={<MessageOutlined style={{ fontSize: '16px' }} />}
+            style={{
+              width: 36,
+              height: 36,
+            }}
+          />
+          <Button
+            type='text'
+            onClick={toggleFullscreen}
+            icon={
+              isFullscreen ? (
+                <FullscreenExitOutlined style={{ fontSize: '16px' }} />
+              ) : (
+                <FullscreenOutlined style={{ fontSize: '16px' }} />
+              )
+            }
+            style={{
+              width: 36,
+              height: 36,
+            }}
+          />
+          <Button
+            type='text'
+            icon={
+              <SettingOutlined
+                spin
+                style={{ fontSize: '16px' }}
+              />
+            }
+            style={{
+              width: 36,
+              height: 36,
+            }}
+          />
+          <Avatar src={avatarImage} />
+        </Flex>
+      </Flex>
     </Header>
   );
 };
